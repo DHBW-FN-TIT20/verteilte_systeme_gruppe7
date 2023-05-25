3e02abd3ed7845ac40550754c6d18c624018b886
philippthuemler <philipp.thuemler@t-online.de>
2023-05-25 20:34:45 +0200

    :label: add toString() function to datatype T_TopicStatus and add unit test

--- inc/global/T_TopicStatus.h
+++ inc/global/T_TopicStatus.h
@@ -13,12 +13,27 @@
  * Include Header Files
  *************************************************************************************************/
 #include "T_SubscriberList.h"
+#include "action_status_enum.h"
 
 /**************************************************************************************************
  * Public - typedefs / structs / enums
  *************************************************************************************************/
-typedef struct {
-  bool                IsTopicExistent;
+typedef struct T_TopicStatusType {
   int32_t             Timestamp;
   T_SubscriberList    SubscriberList_t;
+  ACTION_STATUS_ENUM  ActionStatus;
+
+  std::string toString() {
+    std::string result = "Timestamp: " + std::to_string(Timestamp) + "; Subscriber: ";
+
+    for(size_t i = 0; i < SubscriberList_t.size(); i++) {
+      result += SubscriberList_t.at(i);
+
+      result += (i != SubscriberList_t.size() - 1)? ", " : "; ";
+    }
+
+    result += "Action Status: " + ActionStatus.toString();
+
+    return result;
+  }
 } T_TopicStatus;
---
+++ tests/test_topic_status.cpp
@@ -0,0 +1,43 @@
+/**
+  *************************************************************************************************
+  * @file    test_topic_status.cpp
+  * @author  Christoph Koßlowski, Lukas Adrion, Thibault Rey, Ralf Ehli, Philipp Thümler
+  * @date    24-May-2023
+  * @brief   Unit tests for class ACTION_STATUS_ENUM
+  *************************************************************************************************
+  */
+
+ /**************************************************************************************************
+ * Include Header Files
+ *************************************************************************************************/
+#include "T_TopicStatus.h"
+#include "action_status_enum.h"
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <string>
+
+ /*************************************************************************************************
+ * Unit tests
+ *************************************************************************************************/
+
+void TEST_toString() {
+  int32_t tempTimestamp = 12345678;
+  T_SubscriberList tempSubscriberList = {"subscriber1", "subscriber2", "subscriber3"};
+  ACTION_STATUS_ENUM tempActionStatus = ACTION_STATUS_ENUM::STATUS_OK;
+  
+  T_TopicStatus testTopicStatus_t = {
+    tempTimestamp,
+    tempSubscriberList,
+    tempActionStatus
+  };
+  assert(testTopicStatus_t.toString() == "Timestamp: 12345678; Subscriber: subscriber1, subscriber2, subscriber3; Action Status: STATUS_OK");
+}
+
+
+int main() {
+  TEST_toString();
+
+  std::cout << "All tests for struct T_TopicStatus passed" << std::endl;
+  return 0;
+}

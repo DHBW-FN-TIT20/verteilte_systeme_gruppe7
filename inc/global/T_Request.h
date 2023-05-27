7cfdd2219e7ed7ebc78af4f7e4ab12a266735f3c
Thibse <thibault.rey@outlook.com>
2023-05-27 10:15:54 +0000

    :bug: fix include action enum
    - fix include action enum
    - add brief description
    - update description text

--- inc/global/T_Request.h
+++ inc/global/T_Request.h
@@ -2,8 +2,8 @@
   *************************************************************************************************
   * @file    T_Request.h
   * @author  Christoph Koßlowski, Lukas Adrion, Thibault Rey, Ralf Ehli, Philipp Thümler
-  * @date    24-May-2023
-  * @brief   
+  * @date    27-May-2023
+  * @brief   Type definition for request for communication between publisher/subscriber and broker 
   *************************************************************************************************
   */
 
@@ -14,14 +14,14 @@
  *************************************************************************************************/
 #include <map>
 #include <string>
-#include "T_Action.h"
+#include "action_enum.h"
 
 
 /**************************************************************************************************
  * Public - typedefs / structs / enums
  *************************************************************************************************/
 typedef struct {
-  T_Action                                Action_t;
+  ACTION_ENUM                             Action_t;
   std::map<std::string, std::string>      ParameterList;
   int32_t                                 Timestamp;
-} T_Request; /*!< type definition for message for communication between publisher and broker */
+} T_Request; /*!< type definition for request for communication between publisher/subscriber and broker */

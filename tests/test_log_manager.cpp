4cc302392a21877c0b3fbc216cdff8354fff3750
philippthuemler <philipp.thuemler@t-online.de>
2023-05-31 09:16:48 +0200

    :white_check_mark: add unit test for log manager

--- tests/test_log_manager.cpp
+++ tests/test_log_manager.cpp
@@ -15,6 +15,16 @@
 #include <cassert>
 #include <cstdio>
 #include <regex>
+#include <chrono>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+
+/*************************************************************************************************
+ * Global variabels
+ *************************************************************************************************/
+const std::string TEMP_FILE_NAME = "tempUnitTestFile.txt";
 
 /*************************************************************************************************
  * Test class
@@ -40,28 +50,52 @@ void TestGetTimestampString(void);
  *************************************************************************************************/
 
 void TestConstructor(void) {
-  std::string tempFileName = "tempUnitTestFile.txt";
-
   try {
-    LogManager testLogManagerConstructor(tempFileName);
+    LogManager testLogManagerConstructor(TEMP_FILE_NAME);
   } catch (...) {
     assert(false && "Unexpected exception occurred");
   }
 
-  assert(0U == std::remove(tempFileName.c_str()));
+  assert(0U == std::remove(TEMP_FILE_NAME.c_str()));
 }
 
 void TestAddLogEntry(void) {
+  std::string testLogMessage = "Test log message";
+  std::time_t tempTimestamp;
+  std::string tempTimestampString;
+  try {
+    LogManager testAddLogEntry(TEMP_FILE_NAME);
+    testAddLogEntry.addLogEntry(testLogMessage);
+    tempTimestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::tm* timeinfo = std::localtime(&tempTimestamp);
+    std::ostringstream oss;
+    oss << std::setfill('0') << std::setw(2) << timeinfo->tm_mday << "." << std::setw(2) << (timeinfo->tm_mon + 1U) << "." << (timeinfo->tm_year + 1900U) << " ";
+    oss << std::setw(2) << timeinfo->tm_hour << ":" << std::setw(2) << timeinfo->tm_min << ":" << std::setw(2) << timeinfo->tm_sec << ": ";
+    tempTimestampString = oss.str();
+  } catch(...) {
+    assert(false && "Unexpected exception occcurred");
+  }
 
+  std::string lineBuffer, tempLog;
+  std::ifstream readFile(TEMP_FILE_NAME);
+
+  if(readFile.is_open()) {
+    while(std::getline(readFile, lineBuffer)) {
+      tempLog += lineBuffer;
+    }
+    readFile.close();
+  }
+
+  std::string tempExpectedString = tempTimestampString + testLogMessage;
+  assert(tempLog == tempExpectedString);
 }
 
 void TestGetTimestampString(void) {
-  std::string tempFileName = "tempUnitTestFile.txt";
-  TestLogManager testGetTimestampString(tempFileName);
+  TestLogManager testGetTimestampString(TEMP_FILE_NAME);
   std::string testTimestamp = testGetTimestampString.getTimestampString();
   std::regex tempTimestampPattern(R"(\d{2}\.\d{2}\.\d{4} \d{2}:\d{2}:\d{2}: )");
   assert(std::regex_match(testTimestamp, tempTimestampPattern));
-  assert(0U == std::remove(tempFileName.c_str()));
+  assert(0U == std::remove(TEMP_FILE_NAME.c_str()));
 }
 
 int main() {

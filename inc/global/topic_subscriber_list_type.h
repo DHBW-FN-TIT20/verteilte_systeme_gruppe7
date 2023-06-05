a6947a9c86b2d8379567515e5b8698c87387c8ff
philippthuemler <philipp.thuemler@t-online.de>
2023-06-05 08:19:51 +0200

    :label: update file names to match naming convention - change file names to snake case - add suffix "type" to files that define own datatypes

--- inc/global/topic_subscriber_list_type.h
+++ inc/global/topic_subscriber_list_type.h
@@ -1,33 +1,22 @@
-<<<<<<<< HEAD:inc/global/topic_type.h
-/**************************************************************************************************
-  * @file    topic_type.h
-========
 /**
   *************************************************************************************************
   * @file    topic_subscriber_list_type.h
->>>>>>>> 2381ea8 (:label: update file names to match naming convention):inc/global/topic_subscriber_list_type.h
   * @author  Christoph Koßlowski, Lukas Adrion, Thibault Rey, Ralf Ehli, Philipp Thümler
   * @date    05-June-2023
   * @brief   
-  ************************************************************************************************/
+  *************************************************************************************************
+  */
 
 #pragma once
 
 /**************************************************************************************************
  * Include Header Files
  *************************************************************************************************/
+#include <map>
 #include <string>
-<<<<<<<< HEAD:inc/global/topic_type.h
-#include "request_type.h"
-========
->>>>>>>> 2381ea8 (:label: update file names to match naming convention):inc/global/topic_subscriber_list_type.h
 #include "subscriber_list_type.h"
 
 /**************************************************************************************************
  * Public - typedefs / structs / enums
  *************************************************************************************************/
-struct T_Topic {
-  std::string       TopicName;
-  RequestType       Request;
-  T_SubscriberList  SubscriberList;
-};  /*!< type definition for topic */
+typedef std::map<std::string, T_SubscriberList>   T_TopicSubscriberList;    /*!< type definition for list of topics with all their respective subscribers */

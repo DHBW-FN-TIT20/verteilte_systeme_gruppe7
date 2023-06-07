6a4846b516b765be855bb819e521caf2bbd76505
philippthuemler <philipp.thuemler@t-online.de>
2023-06-07 10:46:55 +0200

    :recycle: add basic event sequence to message handler

--- src/broker.cpp
+++ src/broker.cpp
@@ -13,8 +13,8 @@
 /**************************************************************************************************
  * Class implementation
  *************************************************************************************************/
-Broker::Broker(const T_TopicList& topicList) : mTopicList(topicList) {}
 
+/* private/protected member functions */
 std::vector<std::string> Broker::listTopics() const {
   std::vector<std::string> keys;
   keys.reserve(mTopicList.size());
@@ -22,6 +22,22 @@ std::vector<std::string> Broker::listTopics() const {
   for (const auto& pair : mTopicList) {
       keys.push_back(pair.first);
   }
-
   return keys;
+}
+
+/* public member functions */
+Broker::Broker(const T_TopicList& topicList) : mTopicList(topicList) {}
+
+Broker::Broker(void) {}
+
+void Broker::messageHandler(std::shared_ptr<TcpConnection> conn, const std::string message) {
+  //parse message
+  
+  //decide which function to call based on the message's body
+
+  //call the function
+
+  //encode the return value of the function; add the action status; add delimiter and "\n" at the end of the response message
+
+  //send the response back to the client
 }

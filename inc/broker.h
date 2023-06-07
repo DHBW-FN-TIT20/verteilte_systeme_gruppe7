5b0896d1a06356ec7263d7a7a2e8dcb4074bf55a
philippthuemler <philipp.thuemler@t-online.de>
2023-06-07 10:16:05 +0200

    :sparkles: add class prototype for broker
    - add method declarations and comments
    - add message handler method with udpated parameter list

--- inc/broker.h
+++ inc/broker.h
@@ -12,23 +12,81 @@
  *************************************************************************************************/
 /* C++ Libs */
 #include <vector>
+#include <memory>
+#include <chrono>
+#include <ctime>
 
 /* Own Libs / datatypes */
 #include "topic_list_type.h"
+#include "topic_status_type.h"
+#include "tcp/tcp_server.hpp"
 
 /**************************************************************************************************
  * Public - Class prototype
  *************************************************************************************************/
 class Broker{
-  protected:
+  private:
     T_TopicList mTopicList;
 
+  protected:
     /**
-     * @brief lists all available topics
-    */
-    std::vector<std::string> listTopics() const;
+     * @brief Add subscriber to subscriber list of the given topic
+     * 
+     * @param topicName 
+     * @return ActionStatusType 
+     */
+    ActionStatusType subscribeTopic(std::string topicName);
+
+    /**
+     * @brief Delete subscriber from subscriber list of the giben topic.
+     * If subscriber list is empty after that, delete the topic.
+     * 
+     * @param topicName Topic to unsubscribe from
+     * @return ActionStatusType Action status
+     */
+    ActionStatusType unsubscribeTopic(std::string topicName);
+
+    /**
+     * @brief Publish a topic with the given message
+     * 
+     * @param topicName Topic to publish on
+     * @param message Message to publish
+     * @return ActionStatusType Action status
+     */
+    ActionStatusType publishTopic(std::string topicName, std::string &message);
+
+    /**
+     * @brief Get a list of all existing topics
+     * 
+     * @return T_TopicList List of all topics
+     */
+    std::vector<std::string> listTopics(void) const;
+
+    /**
+     * @brief Get the status of the given topic
+     * 
+     * @param topicName Topic to get status from
+     * @return T_TopicStatus Status of the topic
+     */
+    T_TopicStatus getTopicStatus(std::string topicName) const;
+
+    /**
+     * @brief Update the topic by sending it to all subscribers of the topic with the given message and timestamp
+     * 
+     * @param topicName Topic to update
+     * @param message Message to send
+     * @param timestamp Timestamp of the current topic update
+     */
+    void updateTopic(std::string topicName, std::string &message, std::time_t timestamp);
 
 
   public:
+    /**
+     * @brief Construct a new Broker object
+     * 
+     * @param topicList 
+     */
     Broker(const T_TopicList& topicList);
+
+    void messageHandler(std::shared_ptr<TcpConnection> conn, const std::string message);
 };

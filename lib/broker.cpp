81125ef90bd9a1da641cae27de4168b97bb52d3f
philippthuemler <philipp.thuemler@t-online.de>
2023-06-07 19:58:21 +0200

    :truck: move class implementations to lib folder
    - move implementation of classes broker, subscriber and publisher to
      /lib folder
    - add files broker_main.cpp, subscriber_main.cpp and publisher_main.cpp

--- lib/broker.cpp
+++ lib/broker.cpp
@@ -1,7 +1,11 @@
 /**************************************************************************************************
  * @file    broker.cpp
  * @author  Christoph Koßlowski, Lukas Adrion, Thibault Rey, Ralf Ehli, Philipp Thümler
+<<<<<<< HEAD
  * @date    08-June-2023
+=======
+ * @date    07-June-2023
+>>>>>>> 9631a70 (:truck: move class implementations to lib folder)
  * @brief   Implementation for class Broker
  *************************************************************************************************/
 
@@ -15,15 +19,24 @@
  *************************************************************************************************/
 
 /* private/protected member functions */
+<<<<<<< HEAD
 bool Broker::isTopicExistent(const std::string &topicName) const {
   return mTopicList.count(topicName) > 0U;
 }
 
 bool Broker::hasSubscriber(const std::string &topicName, const T_Subscriber &subscriber) const {
+=======
+bool Broker::isTopicExistent(std::string &topicName) const {
+  return mTopicList.count(topicName) > 0U;
+}
+
+bool Broker::hasSubscriber(std::string &topicName, T_Endpoint &subscriber) const {
+>>>>>>> 9631a70 (:truck: move class implementations to lib folder)
   T_SubscriberList subscriberList = mTopicList.at(topicName).SubscriberList;
   return std::find(subscriberList.begin(), subscriberList.end(), subscriber) != subscriberList.end();
 }
 
+<<<<<<< HEAD
 ActionStatusType Broker::subscribeTopic(const std::string &topicName, const T_Subscriber &subscriber) {
   std::lock_guard<std::mutex> lock(mTopicListMutex);
 
@@ -33,13 +46,24 @@ ActionStatusType Broker::subscribeTopic(const std::string &topicName, const T_Su
     } else {
       return ActionStatusType::INTERNAL_ERROR;  //subscriber already subscribed to this topic
     }
+=======
+ActionStatusType Broker::subscribeTopic(std::string topicName, T_Endpoint subscriber) {
+  std::lock_guard<std::mutex> lock(mTopicListMutex);
+
+  if(isTopicExistent(topicName)) {
+    mTopicList.at(topicName).SubscriberList.push_back(subscriber);
+>>>>>>> 9631a70 (:truck: move class implementations to lib folder)
   } else {
     return ActionStatusType::TOPIC_NON_EXISTENT;
   }
   return ActionStatusType::STATUS_OK;
 }
 
+<<<<<<< HEAD
 ActionStatusType Broker::unsubscribeTopic(const std::string &topicName, const T_Subscriber &subscriber) {
+=======
+ActionStatusType Broker::unsubscribeTopic(std::string topicName, T_Endpoint subscriber) {
+>>>>>>> 9631a70 (:truck: move class implementations to lib folder)
   std::lock_guard<std::mutex> lock(mTopicListMutex);
 
   if(isTopicExistent(topicName)) {
@@ -59,6 +83,7 @@ ActionStatusType Broker::unsubscribeTopic(const std::string &topicName, const T_
 
 }
 
+<<<<<<< HEAD
 ActionStatusType Broker::publishTopic(RequestType &requestFromPublisher) {
   std::lock_guard<std::mutex> lock(mTopicListMutex);
 
@@ -69,11 +94,24 @@ ActionStatusType Broker::publishTopic(RequestType &requestFromPublisher) {
     updateTopic(requestFromPublisher);  //publish updated topic
   } else {
     mTopicList.insert(std::pair<std::string, T_Topic>(topicName, {topicName, requestFromPublisher, {}}));   //add new topic to list
+=======
+ActionStatusType Broker::publishTopic(std::string topicName, [[maybe_unused]]std::string &message) const {
+  std::lock_guard<std::mutex> lock(mTopicListMutex);
+
+  if(isTopicExistent(topicName)) {
+    //generate timestamp and call update topic(), then return success
+  } else {
+    return ActionStatusType::TOPIC_NON_EXISTENT;
+>>>>>>> 9631a70 (:truck: move class implementations to lib folder)
   }
   return ActionStatusType::STATUS_OK;
 }
 
+<<<<<<< HEAD
 std::vector<std::string> Broker::listTopics(void) const {
+=======
+std::vector<std::string> Broker::listTopics() const {
+>>>>>>> 9631a70 (:truck: move class implementations to lib folder)
   std::lock_guard<std::mutex> lock(mTopicListMutex);
   std::vector<std::string> keys;
   
@@ -85,6 +123,7 @@ std::vector<std::string> Broker::listTopics(void) const {
   return keys;
 }
 
+<<<<<<< HEAD
 T_TopicStatus Broker::getTopicStatus(const std::string &topicName) const {
   std::lock_guard<std::mutex> lock(mTopicListMutex);
   if(isTopicExistent(topicName)) {
@@ -122,12 +161,37 @@ Broker::~Broker(void) {
 }
 
 void Broker::messageHandler(std::shared_ptr<TcpConnection> conn, const std::string message) {
+=======
+T_TopicStatus Broker::getTopicStatus([[maybe_unused]]std::string topicName) const {
+  std::lock_guard<std::mutex> lock(mTopicListMutex);
+  //check if topic exists
+    //yes: return T_Topic object from topic list that matches given topicName
+    //no: return respective error type
+  
+  return {1,{},ActionStatusType::STATUS_OK};
+}
+
+void Broker::updateTopic([[maybe_unused]]std::string topicName, [[maybe_unused]]std::string &message, [[maybe_unused]]std::time_t timestamp) const {
+  //send request (RequestType) with topicName, message and timestamp to every subscriber
+  //in the subscriber list of the given topic using udp!
+}
+
+/* public member functions */
+Broker::Broker(void) {}
+
+Broker::Broker(const T_TopicList &topicList) : mTopicList(topicList) {}
+
+Broker::~Broker(void) {}
+
+void Broker::messageHandler(std::shared_ptr<TcpConnection> conn, [[maybe_unused]]const std::string message) {
+>>>>>>> 9631a70 (:truck: move class implementations to lib folder)
   tcp::endpoint endpoint = conn->socket().remote_endpoint();
   T_Endpoint clientEndpoint {
     endpoint.address().to_string(),
     std::to_string(endpoint.port())
   };
 
+<<<<<<< HEAD
   /* display request on terminal */
   std::cout << mLogger.getTimestampString() << clientEndpoint.toString() << " >>" << message << "<<" << std::endl;
 
@@ -188,4 +252,15 @@ void Broker::signalHandler(int signum) {
     std::cout << "TCP-Server closed" << std::endl;
     exit(signum);
   }
+=======
+  //parse message
+
+  //decide which function to call based on the message's body
+
+  //call the function
+
+  //encode the return value of the function; add the action status; add delimiter and "\n" at the end of the response message
+
+  //send the response back to the client
+>>>>>>> 9631a70 (:truck: move class implementations to lib folder)
 }

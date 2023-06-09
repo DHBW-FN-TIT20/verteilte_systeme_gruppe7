6222566ada3098f48869da565073e48ccafe23a2
philippthuemler <philipp.thuemler@t-online.de>
2023-06-09 10:24:02 +0200

    :recycle: adjust function templates for sendRequest
    - update parameter list to match TcpClient class

--- inc/send_request.hpp
+++ inc/send_request.hpp
@@ -1,8 +1,8 @@
 /**************************************************************************************************
- * @file    send_request.cpp
+ * @file    send_request.hpp
  * @author  Christoph Koßlowski, Lukas Adrion, Thibault Rey, Ralf Ehli, Philipp Thümler
- * @date    05-June-2023
- * @brief   
+ * @date    09-June-2023
+ * @brief   Implementation for sendRequest.
  *************************************************************************************************/
 
 #pragma once
@@ -11,34 +11,25 @@
  * Include Header Files
  *************************************************************************************************/
 /* C++ Libs */
+
+
 /* Own Libs / datatypes */
-#include "message_parser.h"
 #include "request_type.h"
+
+#include "message_parser.h"
+#include "tcp/tcp_client.hpp"
 
 /**************************************************************************************************
  * Public - Function implementation
  *************************************************************************************************/
 
-/**
- * @brief send a request and expect an answer
- * 
- * @tparam T expected return type for the response
- * @param request request to be converted to json and sent to the network
- * @param pt_NetworkHandler Pointer to network handler to be used for the transmission
- * @return T response without action status
- */
 template<typename T>
-T sendRequest(RequestType request, INetworkHandler* pt_NetworkHandler) {
+T sendRequest(std::shared_ptr<TcpClient> client, RequestType &request) {
   //normal implementation
+  
 }
 
-/**
- * @brief send a request without expecting an answer (message)
- * 
- * @param request request to be converted to json and sent to the network
- * @param pt_NetworkHandler Pointer to network handler to be used for the transmission
- */
 template<>
-void sendRequest<void>(RequestType request, INetworkHandler* pt_NetworkHandler) {
+void sendRequest<void>(std::shared_ptr<TcpClient> client, RequestType &request) {
   //void-specific implementation
 }

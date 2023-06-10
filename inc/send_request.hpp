a712d181c34e99a28707c49922c6c85285f187ba
philippthuemler <philipp.thuemler@t-online.de>
2023-06-10 15:21:05 +0200

    :art: improve implementation of send request
    - add send request functions to namespace network
    - remove action type verification from functions

--- inc/send_request.hpp
+++ inc/send_request.hpp
@@ -19,7 +19,6 @@
 #include <sstream>
 #include <stdexcept>
 
-
 /* Own Libs / datatypes */
 #include "request_type.h"
 #include "action_type.h"
@@ -34,118 +33,86 @@
 /**************************************************************************************************
  * Public - Function implementation
  *************************************************************************************************/
-std::vector<std::string> splitAndRemoveNewline(std::string str) {
-  /* remove \n from end of the string if it exists */
-  if(!str.empty() && str[str.length() - 1] == '\n') {
-    str.erase(str.length() - 1);
+namespace network {
+
+  std::vector<std::string> splitAndRemoveNewline(std::string str) {
+    /* remove \n from end of the string if it exists */
+    if(!str.empty() && str[str.length() - 1] == '\n') {
+      str.erase(str.length() - 1);
+    }
+
+    /* split the string at the delimiter ";" */
+    std::vector<std::string> result;
+    std::stringstream ss(str);
+    std::string item;
+
+    while(std::getline(ss, item, ';')) {
+      result.push_back(item);
+    }
+
+    return result;
   }
 
-  /* split the string at the delimiter ";" */
-  std::vector<std::string> result;
-  std::stringstream ss(str);
-  std::string item;
 
-  while(std::getline(ss, item, ';')) {
-    result.push_back(item);
+  /**
+   * @brief Send a request to the TCP server and return the response
+   * 
+   * @param client Open connection to the server
+   * @param request Request to be sent to the server
+   * @return T Type to parse response into. Provided by the caller function.
+   */
+  template<typename T>
+  T sendRequest(std::shared_ptr<TcpClient> client, RequestType &request) {
+    MessageParser messageParser;
+    LogManager logger("log.txt");
+
+    /* send request to server */
+    client->sendMessage(messageParser.encodeObject(request));
+    std::string clientEndpointStr = client->socket().local_endpoint().address().to_string() + ":" + std::to_string(client->socket().local_endpoint().port());
+
+
+    /* handle response from server */
+    std::string responseStr = client->waitForSingleResponse();    //read single action status
+    std::vector<std::string> responseSubStr = splitAndRemoveNewline(responseStr);    //remove "\n" from resonse and split it at ";"
+    ActionStatusType actionStatus = messageParser.decodeObject<ActionStatusType>(responseSubStr.at(0));
+
+    if(responseSubStr.size() > 1 && actionStatus == ActionStatusType::STATUS_OK) {
+      T result = messageParser.decodeObject<T>(responseSubStr.at(1));
+      logger.addLogEntry(clientEndpointStr + request.mAction.toString() + " successful; Response: >>" + result.toString() + "<<");
+      return result;
+    }
+    std::cout << logger.getTimestampString() << actionStatus.toString() << std::endl;
+    logger.addLogEntry(clientEndpointStr + " >>" + actionStatus.toString()) + "<<";
+    throw std::runtime_error("Action status: " + actionStatus.toString());
   }
 
-  return result;
-}
 
+  /**
+   * @brief Send a request to the TCP server. Only expect an action status as response and handle it.
+   * 
+   * @param client Open connection to the server
+   * @param request Request to be sent to the server
+   */
+  void sendRequestWithoutResponse(std::shared_ptr<TcpClient> client, RequestType &request) {
+    MessageParser messageParser;
+    LogManager logger("log.txt");
 
+    std::string requestStr = messageParser.encodeObject(request);
+    std::string clientEndpointStr = client->socket().local_endpoint().address().to_string() + ":" + std::to_string(client->socket().local_endpoint().port());
 
-/**
- * @brief Send a request to the TCP server and return the response
- * 
- * @param client Open connection to the server
- * @param request Request to be sent to the server
- * @return T Type to parse response into. Provided by the caller function.
- */
-template<typename T>
-T sendRequest(std::shared_ptr<TcpClient> client, RequestType &request) {
-  MessageParser messageParser;
-  LogManager logger("log.txt");
+    /* send request to server */
+    client->sendMessage(requestStr);
+    std::string responseStr = client->waitForSingleResponse();    //get action status
+    std::vector<std::string> responseSubStr = splitAndRemoveNewline(responseStr);    //remove "\n" from reponse
+    ActionStatusType actionStatus = messageParser.decodeObject<ActionStatusType>(responseSubStr.at(0));
 
-  std::string clientEndpointStr = client->socket().local_endpoint().address().to_string() + ":" + std::to_string(client->socket().local_endpoint().port());
-  std::string responseStr;
-  std::vector<std::string> responseSubStr;
-  T result;
-
-  /* send request to server */
-  client->sendMessage(messageParser.encodeObject(request));
-
-
-  /* handle response from server */
-  switch(request.mAction) {
-    case ActionType::LIST_TOPICS:
-    case ActionType::GET_TOPIC_STATUS:
-      responseStr = client->waitForSingleResponse();    //read single action status
-      responseSubStr = splitAndRemoveNewline(responseStr);    //remove "\n" from resonse and split it at ";"
-      if(responseSubStr.size() > 1) {   //at least 2 elements were in the response
-        if(messageParser.decodeObject<ActionStatusType>(responseSubStr.at(0)) == ActionStatusType::STATUS_OK) {   //no error -> response is complete
-          logger.addLogEntry(clientEndpointStr + ">>" + result.toString() + "<<");
-          result = messageParser.decodeObject<T>(responseSubStr.at(1));
-          return result;
-        } else {
-          std::cout << logger.getTimestampString() << messageParser.decodeObject<ActionStatusType>(responseSubStr.at(0)).toString() << std::endl;
-          logger.addLogEntry(clientEndpointStr + " " + ">>" + messageParser.decodeObject<ActionStatusType>(responseSubStr.at(0)).toString()) + "<<";
-          throw std::runtime_error("Action status: " + messageParser.decodeObject<ActionStatusType>(responseSubStr.at(0)).toString());
-        }
-      } else {
-        std::cout << logger.getTimestampString() << "INVALID_PARAMETERS" << std::endl;
-        logger.addLogEntry(clientEndpointStr + " >>INVALID_PARAMETERS<<");
-        throw std::runtime_error("INVALID_PARAMETERS");
-      }
-      break;
-    default:
-      logger.addLogEntry(clientEndpointStr + " >>INTERNAL_ERROR<<");
-      throw std::runtime_error("INTERNAL_ERROR");
-      break;
+    if(responseSubStr.size() == 1 && actionStatus == ActionStatusType::STATUS_OK) {
+      std::cout << logger.getTimestampString() << request.mAction.toString() << " successful" << std::endl;
+      logger.addLogEntry(clientEndpointStr + " " + request.mAction.toString() + " successful");
+    }
+    std::cout << logger.getTimestampString() << actionStatus.toString() << std::endl;
+    logger.addLogEntry(clientEndpointStr + " " + ">>" + actionStatus.toString() + "<<");
+    throw std::runtime_error("Action status: " + actionStatus.toString());
   }
-}
 
-
-/**
- * @brief Send a request to the TCP server. Only expect an action status as response and handle it.
- * 
- * @param client Open connection to the server
- * @param request Request to be sent to the server
- */
-void sendRequestWithoutResponse(std::shared_ptr<TcpClient> client, RequestType &request) {
-  MessageParser messageParser;
-  LogManager logger("log.txt");
-
-  std::string requestStr = messageParser.encodeObject(request);
-  std::string responseStr, errorStr;
-  std::vector<std::string> responseSubStr;
-  std::string clientEndpointStr = client->socket().local_endpoint().address().to_string() + ":" + std::to_string(client->socket().local_endpoint().port());
-
-  /* send request to server */
-  client->sendMessage(requestStr);
-  responseStr = client->waitForSingleResponse();    //get action status
-
-  switch(request.mAction) {
-    case ActionType::SUBSCRIBE_TOPIC:
-    case ActionType::UNSUBSCRIBE_TOPIC:
-    case ActionType::PUBLISH_TOPIC:
-      responseSubStr = splitAndRemoveNewline(responseStr);    //remove "\n" from reponse
-      if(responseSubStr.size() == 1) {
-        if(messageParser.decodeObject<ActionStatusType>(responseSubStr.at(0)) == ActionStatusType::STATUS_OK) {
-          std::cout << logger.getTimestampString() << request.mAction.toString() << " successful" << std::endl;
-          logger.addLogEntry(clientEndpointStr + " " + request.mAction.toString() + " successful");
-        } else {
-          std::cout << logger.getTimestampString() << messageParser.decodeObject<ActionStatusType>(responseSubStr.at(0)).toString() << std::endl;
-          logger.addLogEntry(clientEndpointStr + " " + ">>" + messageParser.decodeObject<ActionStatusType>(responseSubStr.at(0)).toString() + "<<");
-          throw std::runtime_error("Action status: " + messageParser.decodeObject<ActionStatusType>(responseSubStr.at(0)).toString());
-        }
-      } else {
-        std::cout << logger.getTimestampString() << "INVALID_PARAMETERS" << std::endl;
-        logger.addLogEntry(clientEndpointStr + " >>INVALID_PARAMETERS<<");
-        throw std::runtime_error("INVALID_PARAMETERS");
-      }
-    default:
-      logger.addLogEntry(clientEndpointStr + " >>INTERNAL_ERROR<<");
-      throw std::runtime_error("INTERNAL_ERROR");
-      break;
-  }
 }

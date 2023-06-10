42118edd224dff546b4c3277a4ce5657b566df28
philippthuemler <philipp.thuemler@t-online.de>
2023-06-10 04:13:20 +0200

    :adhesive_bandage: add run method for client
    - start a client using client->run()

--- lib/biglib/tcp/tcp_client.hpp
+++ lib/biglib/tcp/tcp_client.hpp
@@ -83,6 +83,10 @@ class TcpClient : public std::enable_shared_from_this<TcpClient> {
       });
     }
 
+    void run(void) {
+      mIoContext.run();
+    }
+
     void close(void) {
       mSocket.close();
     }
